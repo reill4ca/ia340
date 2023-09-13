40a5e6c29534733e8228caa6eb745a512d6e4a55
reill4ca <122939306+reill4ca@users.noreply.github.com>
2023-09-13 16:21:14 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -24,4 +24,44 @@ values
 
 select * from course
 
+--Q2.4
+insert into enroll(s_email,c_number)
+values 
+		('s1@jmu.edu','c1'),
+		('s2@jmu.edu','c1'),
+		('s3@jmu.edu','c1'),
+		('s4@jmu.edu','c2'),
+		('s2@jmu.edu','c3'),
+		('s3@jmu.edu','c3');
 
+select * from enroll
+
+--Q2.5
+insert into professor(p_email,p_name,office)
+values
+		('p3@jmu.edu','p3','o3')
+
+select * from professor
+
+insert into course(c_number,c_name,room,p_email)
+values
+		('c4','facebook','r1','p3@jmu.edu')
+
+select * from course
+
+/*You must modify the professor table first because the course table references the professor table.*/
+
+--Q2.6
+
+update course
+set p_email = 'p3@jmu.edu'
+where p_email = 'p1@jmu.edu'
+
+select * from course
+
+delete from professor
+where p_email = 'p1@jmu.edu'
+
+select * from professor
+
+/* You must update course first because otherwise the row with p1's courses will be deleted.*/
