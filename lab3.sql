e17535df26c0b4d105abab5f0386c0eb5eb2352e
reill4ca <122939306+reill4ca@users.noreply.github.com>
2023-09-20 16:02:27 -0400

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -65,3 +65,27 @@ where p_email = 'p1@jmu.edu'
 select * from professor
 
 /* You must update course first because otherwise the row with p1's courses will be deleted.*/
+
+--Q2.7
+select * from enroll
+
+--Q2.8
+select c_number,COUNT(*) as num_student
+from enroll
+group by c_number 
+order by num_student desc
+limit 1
+
+--Q2.9
+select professor.p_name, course.c_name
+from professor
+inner join course
+on professor.p_email = course.p_email
+
+--Q2.10
+select professor.p_name, COUNT(course.c_number) as num_course_taught
+from professor
+inner join course on professor.p_email = course.p_email
+group by professor.p_name
+order by num_course_taught desc
+limit 1;
